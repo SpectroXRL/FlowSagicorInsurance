2a1aa8e89cfe6d2eb38b0addfe1e3672a9d28629
AmazTyo <91347253+AmazTyo@users.noreply.github.com>
2024-04-16 21:25:36 -0500

    Added AccountDetails

---
+++ FlowSagicorInsurance/AccountDetails.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AccountDetails.aspx.cs" Inherits="FlowSagicorInsurance.AccountDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<main aria-labelledby="title">
+        <h2 id="title">Your Account Details</h2>
+        <p class="lead">Here you can view your existing accounts and their current balances.</p>
+        
+        <asp:GridView ID="AccountDetailsGrid" runat="server" AutoGenerateColumns="False" CssClass="table">
+            <Columns>
+                <asp:BoundField DataField="AccountNumber" HeaderText="Account Number" />
+                <asp:BoundField DataField="AccountType" HeaderText="Account Type" />
+                <asp:BoundField DataField="Balance" HeaderText="Current Balance" />
+            </Columns>
+        </asp:GridView>
+    </main>
+</asp:Content>
---
+++ FlowSagicorInsurance/AccountDetails.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.AspNet.Identity;
+using System.Web.UI.WebControls;
+
+namespace FlowSagicorInsurance
+{
+
+    public partial class AccountDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadAccountDetails();
+            }
+        }
+        private void LoadAccountDetails()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT AccountNumber, AccountType, Balance FROM AccountDetails WHERE UserId = @UserId"))
+                {
+                    cmd.Parameters.AddWithValue("@UserId", Context.User.Identity.GetUserId());
+                    cmd.Connection = con;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        AccountDetailsGrid.DataSource = dt;
+                        AccountDetailsGrid.DataBind();
+                    }
+                    con.Close();
+                }
+            }
+        }
+    }
+}
