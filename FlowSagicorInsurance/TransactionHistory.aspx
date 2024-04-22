cb87b0f9260baf4c8f0183835dc08beb02b40418
SpectroXRL <spacespectrumdev@gmail.com>
2024-04-22 02:15:01 -0500

    Payment Changes

---
+++ FlowSagicorInsurance/TransactionHistory.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TransactionHistory.aspx.cs" Inherits="FlowSagicorInsurance.TransactionHistory" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2 id="title">Your Transaction History</h2>
+    <p class="lead">Here you can view your history of transactions made using this account.</p>
+    
+    <asp:GridView ID="TransactionHistoryGrid" runat="server" AutoGenerateColumns="False" CssClass="table" EmptyDataText="You have no transactions.">
+        <Columns>
+            <asp:BoundField DataField="AccountID" HeaderText="Account Number" />
+            <asp:BoundField DataField="Payment" HeaderText="Amount" />
+            <asp:BoundField DataField="FlowID" HeaderText="Flow Account" />
+            <asp:BoundField DataField="SagicorID" HeaderText="Sagicor Account" />
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ FlowSagicorInsurance/TransactionHistory.aspx.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FlowSagicorInsurance
+{
+    public partial class TransactionHistory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindTransactionHistoryGrid();
+            }
+        }
+
+        private void BindTransactionHistoryGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT AccountID, Payment, FlowID, SagicorID FROM [dbo].[Transaction] WHERE AspNetUserID = @AspNetUserID"))
+                {
+                    cmd.Parameters.AddWithValue("@AspNetUserID", Context.User.Identity.GetUserId());
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            TransactionHistoryGrid.DataSource = dt;
+                            TransactionHistoryGrid.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
